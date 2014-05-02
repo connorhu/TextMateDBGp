7135bbee3081460941447754c555230bb09cdea7
Gossler Karoly <connor@connor.hu>
2014-05-02 13:55:05 +0200

    just some typo

--- Classes/TextMateDBGp.m
+++ Classes/TextMateDBGp.m
@@ -44,70 +44,77 @@
 
 @implementation TextMateDBGp
 
-+ (void)load {
-  // Setup defaults
-  NSColor *activeColor = [NSColor colorWithCalibratedRed:0.867f green:0.894f blue:0.918f alpha:1.0f];
-  NSColor *idleColor = [NSColor colorWithCalibratedRed:0.929f green:0.929f blue:0.929f alpha:1.0f];
-  NSDictionary *defaults = [[NSDictionary alloc] initWithObjectsAndKeys:
-                            [NSKeyedArchiver archivedDataWithRootObject:activeColor], kMDSidebarBackgroundColorActiveKey,
-                            [NSKeyedArchiver archivedDataWithRootObject:idleColor], kMDSidebarBackgroundColorIdleKey,
-                            nil];
-  
-  [[NSUserDefaults standardUserDefaults] registerDefaults:defaults];
-  [[NSUserDefaultsController sharedUserDefaultsController] setInitialValues:defaults];
-  [defaults release];
-  
-  [OakProjectController jr_swizzleMethod:@selector(windowDidLoad) withMethod:@selector(MD_repl_windowDidLoad) error:NULL];
-  [OakProjectController jr_swizzleMethod:@selector(windowWillClose:) withMethod:@selector(MD_repl_windowWillClose:) error:NULL];
-  [OakProjectController jr_swizzleMethod:@selector(openProjectDrawer:) withMethod:@selector(MD_repl_openProjectDrawer:) error:NULL];
-  [OakProjectController jr_swizzleMethod:@selector(revealInProject:) withMethod:@selector(MD_repl_revealInProject:) error:NULL];
-  [OakProjectController jr_swizzleMethod:@selector(validateMenuItem:) withMethod:@selector(TD_validateMenuItem:) error:NULL];
++ (void)load
+{
+    // Setup defaults
+    NSColor *activeColor = [NSColor colorWithCalibratedRed:0.867f green:0.894f blue:0.918f alpha:1.0f];
+    NSColor *idleColor = [NSColor colorWithCalibratedRed:0.929f green:0.929f blue:0.929f alpha:1.0f];
+    NSDictionary *defaults = [[NSDictionary alloc] initWithObjectsAndKeys:
+                              [NSKeyedArchiver archivedDataWithRootObject:activeColor], kMDSidebarBackgroundColorActiveKey,
+                              [NSKeyedArchiver archivedDataWithRootObject:idleColor], kMDSidebarBackgroundColorIdleKey,
+                              nil];
+    
+    [[NSUserDefaults standardUserDefaults] registerDefaults:defaults];
+    [[NSUserDefaultsController sharedUserDefaultsController] setInitialValues:defaults];
+    [defaults release];
+    
+    [OakProjectController jr_swizzleMethod:@selector(windowDidLoad) withMethod:@selector(MD_repl_windowDidLoad) error:NULL];
+    [OakProjectController jr_swizzleMethod:@selector(windowWillClose:) withMethod:@selector(MD_repl_windowWillClose:) error:NULL];
+    [OakProjectController jr_swizzleMethod:@selector(openProjectDrawer:) withMethod:@selector(MD_repl_openProjectDrawer:) error:NULL];
+    [OakProjectController jr_swizzleMethod:@selector(revealInProject:) withMethod:@selector(MD_repl_revealInProject:) error:NULL];
+    [OakProjectController jr_swizzleMethod:@selector(validateMenuItem:) withMethod:@selector(TD_validateMenuItem:) error:NULL];
 }
 
 #pragma mark Class Methods
 
-+ (NSBundle *)pluginBundle {
++ (NSBundle *)pluginBundle
+{
 	return [NSBundle bundleForClass:[self class]];
 }
 
 
-+ (NSImage *)bundledImageWithName:(NSString *)imageName {
++ (NSImage *)bundledImageWithName:(NSString *)imageName
+{
 	NSBundle *pluginBundle = [[self class] pluginBundle];
 	return [[[NSImage alloc] initWithContentsOfFile:[pluginBundle pathForResource:imageName ofType:@"png"]] autorelease];
 }
 
 
-+ (TDSplitView *)makeSplitViewWithMainView:(NSView *)contentView sideView:(TDSidebar *)sideView {
-  TDSplitView *splitView = [[TDSplitView alloc] initWithFrame:[contentView frame] mainView:contentView sideView:sideView];
-  return [splitView autorelease];
++ (TDSplitView *)makeSplitViewWithMainView:(NSView *)contentView sideView:(TDSidebar *)sideView
+{
+    TDSplitView *splitView = [[TDSplitView alloc] initWithFrame:[contentView frame] mainView:contentView sideView:sideView];
+    return [splitView autorelease];
 }
 
 
 #pragma mark Plugin Hook
 
-- (id)initWithPlugInController:(id<TMPlugInController>)aController {
+- (id)initWithPlugInController:(id<TMPlugInController>)aController
+{
 	if (self = [super init]) {
 		[[[NSApp mainWindow] windowController] MD_splitWindowIfNeeded];
 		[[[NSApp mainWindow] windowController] MD_windowDidBecomeMain:nil];
 		[self _installMenuItems];
 		[self _injectPreferenceMethods];
-  }	
-  return self;
+    }
+    return self;
 }
 
 
 #pragma mark Actions
 
-- (void)toggleSplitViewLayout:(id)sender {
+- (void)toggleSplitViewLayout:(id)sender
+{
 	[[NSNotificationCenter defaultCenter] postNotificationName:@"MDToggleSplitViewLayout" object:nil];
 }
 
 
 #pragma mark Private Methods
 
-- (void)_installMenuItems {
+- (void)_installMenuItems
+{
 	NSMenu *viewMenu = [[[NSApp mainMenu] itemWithTitle:@"View"] submenu];
-	
+    
 	NSMenuItem *showHideDrawerMenuItem = nil;
 	NSInteger drawerMenuItemIndex = 0;
 	
@@ -119,38 +126,40 @@
 			drawerMenuItemIndex = [[viewMenu itemArray] indexOfObject:menuItem];
 		}
 	}
-	
+    
 	NSMenuItem *drawerSubmenuItem =  [[NSMenuItem alloc] initWithTitle:@"Project Drawer" action:nil keyEquivalent:@""];
 	NSMenu *drawerMenu = [[NSMenu alloc] initWithTitle:@"Project Drawer"];
+    
 	[drawerSubmenuItem setSubmenu:drawerMenu];
 	[drawerMenu addItem:settings.toggleSplitViewLayoutMenuItem];
 	[drawerMenu addItem:settings.focusSideViewMenuItem];
-  [drawerMenu addItem:settings.filterInDrawerMenuItem];
-  [drawerMenu addItem:[NSMenuItem separatorItem]];
-  [drawerMenu addItem:settings.navigatorViewMenuItem];
-  [drawerMenu addItem:settings.debuggerViewMenuItem];
-  [drawerMenu addItem:settings.breakpointsViewMenuItem];
-  
+    [drawerMenu addItem:settings.filterInDrawerMenuItem];
+    [drawerMenu addItem:[NSMenuItem separatorItem]];
+    [drawerMenu addItem:settings.navigatorViewMenuItem];
+    [drawerMenu addItem:settings.debuggerViewMenuItem];
+    [drawerMenu addItem:settings.breakpointsViewMenuItem];
+
 	[showHideDrawerMenuItem retain];
 	[viewMenu removeItemAtIndex:drawerMenuItemIndex];
 	[drawerMenu insertItem:showHideDrawerMenuItem atIndex:0];
 	[viewMenu insertItem:drawerSubmenuItem atIndex:drawerMenuItemIndex];
-	
+
 	[drawerSubmenuItem release];
 	[drawerMenu release];
 	[showHideDrawerMenuItem release];
 }
 
 
-- (void)_injectPreferenceMethods {
+- (void)_injectPreferenceMethods
+{
 	MDLog("swapping OakPreferencesManager methods");
 	
-  NSError* error;
-  [OakPreferencesManager jr_swizzleMethod:@selector(toolbarAllowedItemIdentifiers:) withMethod:@selector(MD_toolbarAllowedItemIdentifiers:) error:&error];
-  [OakPreferencesManager jr_swizzleMethod:@selector(toolbarDefaultItemIdentifiers:) withMethod:@selector(MD_toolbarDefaultItemIdentifiers:) error:&error];
-  [OakPreferencesManager jr_swizzleMethod:@selector(toolbarSelectableItemIdentifiers:) withMethod:@selector(MD_toolbarSelectableItemIdentifiers:) error:&error];
-  [OakPreferencesManager jr_swizzleMethod:@selector(selectToolbarItem:) withMethod:@selector(MD_selectToolbarItem:) error:&error];
-  [OakPreferencesManager jr_swizzleMethod:@selector(toolbar:itemForItemIdentifier:willBeInsertedIntoToolbar:) withMethod:@selector(MD_toolbar:itemForItemIdentifier:willBeInsertedIntoToolbar:) error:&error];
+    NSError* error;
+    [OakPreferencesManager jr_swizzleMethod:@selector(toolbarAllowedItemIdentifiers:) withMethod:@selector(MD_toolbarAllowedItemIdentifiers:) error:&error];
+    [OakPreferencesManager jr_swizzleMethod:@selector(toolbarDefaultItemIdentifiers:) withMethod:@selector(MD_toolbarDefaultItemIdentifiers:) error:&error];
+    [OakPreferencesManager jr_swizzleMethod:@selector(toolbarSelectableItemIdentifiers:) withMethod:@selector(MD_toolbarSelectableItemIdentifiers:) error:&error];
+    [OakPreferencesManager jr_swizzleMethod:@selector(selectToolbarItem:) withMethod:@selector(MD_selectToolbarItem:) error:&error];
+    [OakPreferencesManager jr_swizzleMethod:@selector(toolbar:itemForItemIdentifier:willBeInsertedIntoToolbar:) withMethod:@selector(MD_toolbar:itemForItemIdentifier:willBeInsertedIntoToolbar:) error:&error];
 }
 
 @end
